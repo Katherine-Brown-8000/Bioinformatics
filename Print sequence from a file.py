49f77ecb3d076e8a07b8a439915a5bb8867a695a
Katherine Brown <155707632+Katherine-Brown-8000@users.noreply.github.com>
2024-10-18 00:11:26 -0400

    Update Print sequence from a file.py

--- Print sequence from a file.py
+++ Print sequence from a file.py
@@ -1,6 +1,6 @@
-file_name = r"C:\Users\user_name\Desktop\Python code files\lanbda_virus.txt"
+file_name = r"C:\Users\scien\Desktop\Python code files\lanbda_virus.txt"
 
-def read_file(file):
+def read_file(file_name):
     with open(file_name, 'r') as file:
         content = file.read().strip()
         return content
